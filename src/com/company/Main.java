52f0446a26cacacfc87143cf4ca5a64c5c6fca13
slog4 <bogdan.slog4@gmail.com>
2021-08-03 23:24:16 +0300

    Добавил текущую дату

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import com.company.classwork6.Car;
 public class Main {
 
     public static void main(String[] args) {
-        Car car =new Car( "911",2019, "Porsh");
+        Car car =new Car( "911",2019, "Porsche");
         System.out.println( car.toString());
 	// write your code here
     }
